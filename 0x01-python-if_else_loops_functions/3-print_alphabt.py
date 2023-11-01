25796bafc46d8f542f1aab8ebe2e5635d8083b50
Lucianjoki <luciawaweru22@gmail.com>
2023-11-01 02:08:53 -0700

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/pyhon3
+"""Print alphabet in lowercase, not followed by a new line"""
+for letter in range)97,123):
+    if chr(letter) is not in 'eq':
+        print("{}" .format(chr(letter)), end="")
