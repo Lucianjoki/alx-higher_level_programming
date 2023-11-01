3a9b48495f6fac92916df8325e3cb668ddae3b8a
Lucianjoki <luciawaweru22@gmail.com>
2023-11-01 06:09:48 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pytho3
+def uppercase(str):
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+            i = chr(ord(i) - 32)
+        print("{S}".format(i),end="")
+print()
